1ceeff63a19681de7a13654bcec1fd533d720596
Carl John Manigos <manigoscarljohn@gmail.com>
2022-09-02 18:46:46 +0800

    Responsive Navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,10 @@
 // fetch('https://fakestoreapi.com/products')
 //     .then((res) => res.json())
 //     .then((json) => console.log(json));
+
+const menuButton = document.querySelector('.menu-bar');
+const items = document.querySelector('.items');
+
+menuButton.addEventListener('click', function () {
+    items.classList.toggle('active');
+});
